b84cd7383c6e98ed8015ba7a5f38541936f4d5ef
ankit pandit <ankit.pandit@globalids.com>
2019-06-25 17:03:08 +0530

    Login page mockup

--- src/VuetifyPlayground.vue
+++ src/VuetifyPlayground.vue
@@ -1,33 +1,14 @@
 <template>
     <div class="">
-      <v-expand-transition >
-        <div v-if="!open">
-          <v-card >
-            <v-card-text>show
-              show<br>
-              show
-              show<br>
-              show<br>
-              show<br>
-              show<br>
-            </v-card-text>
-            <v-card-actions>            
-              <v-btn @click="open = !open">
-                Toggle
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </div>
-      </v-expand-transition>
-      <child-component v-model="open"></child-component>
+      <login-page></login-page>
     </div>
 </template>
 
 <script>
-import ChildComponent from './ChildComponent.vue';
+import LoginPage from './LoginPage.vue';
 export default {  
   components: {
-    ChildComponent
+    LoginPage
   },
   methods:{
   },
@@ -40,23 +21,17 @@ export default {
 }
 </script>
 
-<style scoped>
-    .profile-img{
-        height: 80px;
-        transform: translateY(-75px);
-    }
-    .profile-img .v-avatar img{
-        border: 4px solid #EFEFEF;
-    }
+<style>
     .test{
         background: red;
         border: 1px solid red !important;
     }
-    .shift{
-      margin-top: 5px;
-      transition: .5s;
+    .test-1{
+        background: green;
+        border: 1px solid green !important;
     }
-    .rotate{
-      transform: rotate(-45deg);
+    .test-2{
+        background: blue;
+        border: 1px solid blue !important;
     }
 </style>
