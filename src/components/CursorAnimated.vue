e3fac4188fc55b73ca0332990c5dfc85b6c1f0b0
Ankit <panditankit1995@gmail.com>
2020-01-18 18:35:29 +0530

    cursor style changed

--- src/components/CursorAnimated.vue
+++ src/components/CursorAnimated.vue
@@ -1,17 +1,16 @@
 <template>
-    <div
-        v-if="cursor"
-        class="cursor-animated"
-        :class="[progress == 0 ? 'cursor-animated--hidden' : '']"
-    >
-        <v-progress-circular
-            size="56"
-            width="2"
-            :value="progress"
-            :color="$vuetify.theme.dark ? '#e0e0e0' : '#212121'"
-            :class="progress == 100 ? 'cursor-animated--rotated' : ''"
-        ></v-progress-circular>
+    <div v-if="cursor">
         <div
+            id="animatedcursor"
+            class="cursor-animated"
+            :class="[
+                progress == 0
+                    ? 'cursor-animated--small'
+                    : 'cursor-animated--large',
+            ]"
+        ></div>
+        <div
+            id="tooltipforcursor"
             class="cursor-tooltip"
             :class="[
                 (cursor.tooltip && cursor.tooltip !== '') ||
@@ -57,8 +56,14 @@ export default {
     },
     methods: {
         handleCursor(e) {
-            const cursor = document.querySelector('.cursor-animated');
-            cursor.style.transform = `translate3d(${e.pageX}px,${e.pageY}px, 0px)`;
+            try {
+                const cursor = document.getElementById('animatedcursor');
+                const tooltip = document.getElementById('tooltipforcursor');
+                cursor.style.transform = `translate3d(${e.pageX}px,${e.pageY}px, 0px)`;
+                tooltip.style.transform = `translate3d(${e.pageX}px,${e.pageY}px, 0px)`;
+            } catch (e) {
+                // catching dom errors
+            }
         },
     },
     mounted() {
@@ -71,29 +76,45 @@ export default {
 </script>
 
 <style>
-.cursor-animated {
-    top: -28px;
-    left: -28px;
+.cursor-animated,
+.cursor-tooltip {
     z-index: 200;
     position: absolute;
     pointer-events: none;
-    will-change: transform;
-    transition: transform 200ms ease-out;
 }
-.cursor-animated--hidden {
-    opacity: 0;
-    transition: opacity 1s;
-    transition-delay: 0.5s;
+.cursor-animated {
+    border-radius: 50%;
+    border: 2px solid white;
+    mix-blend-mode: difference;
+    transform: translate3d(-100px, -100px, 0px);
+}
+.cursor-animated--small {
+    top: -12px;
+    left: -12px;
+    width: 24px;
+    height: 24px;
+}
+.cursor-animated--large {
+    top: -28px;
+    left: -28px;
+    width: 56px;
+    height: 56px;
+    background: white;
+}
+.cursor-animated,
+.cursor-animated--small,
+.cursor-animated--large {
+    will-change: transform, width, height;
+    transition: all 200ms ease-out;
 }
 .cursor-animated--rotated {
     transform: scaleX(-1) rotate(180deg);
 }
 .cursor-tooltip {
     left: 56px;
-    height: auto;
     width: auto;
+    height: auto;
     padding: 16px;
-    position: relative;
     overflow: hidden;
     box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
         0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
@@ -113,10 +134,10 @@ export default {
     width: 0px;
     height: 0px;
     padding: 0px;
+    opacity: 0;
 }
 .cursor-photo,
-.cursor-tooltip,
-.cursor-tooltip--hidden {
-    transition: all 400ms ease;
+.cursor-tooltip {
+    transition: all 200ms ease-out;
 }
 </style>
